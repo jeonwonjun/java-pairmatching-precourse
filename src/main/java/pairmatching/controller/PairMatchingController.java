d0275cd7d618885b732a263f52c6496e1aaa2cfb
전원준 <jwjjwj1960@naver.com>
2026-01-09 16:18:22 +0900

    feat: 전체적인 로직 controller 샐성
    1. menuHandler를 통해 Error가 발생해도 반복할 수 있도록 진행
    2. 페어 매칭, 페어 조회, 페어 초기화를 할 수 있게 호출

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,113 @@
+package pairmatching.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import pairmatching.model.crew.Course;
+import pairmatching.model.crew.Crew;
+import pairmatching.model.crew.Level;
+import pairmatching.model.crew.Mission;
+import pairmatching.model.pair.Pair;
+import pairmatching.model.pair.PairResult;
+import pairmatching.model.pair.PairResults;
+import pairmatching.util.ErrorMessage;
+import pairmatching.util.FileScanner;
+import pairmatching.view.InputValidator;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class PairMatchingController {
+    FileScanner fileScanner = new FileScanner();
+
+    public void run() {
+        PairResults pairResults = new PairResults();
+        while (true) {
+            try {
+                String menuNumber = InputView.readMenu();
+                if (menuNumber.equals("Q")) {
+                    break;
+                }
+                InputValidator.validateEmpty(menuNumber);
+                InputValidator.validateNumeric(menuNumber);
+                handlerMenu(menuNumber, pairResults);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void handlerMenu(String menuNumber, PairResults pairResults) {
+        if (menuNumber.equals("1")) {
+            makePair(pairResults);
+        }
+        if (menuNumber.equals("2")) {
+            searchInfo(pairResults);
+        }
+        if (menuNumber.equals("3")) {
+            pairResults.clearList();
+        }
+    }
+
+    private void makePair(PairResults pairResults) {
+        String input = InputView.readInfo();
+        List<String> inputList = Arrays.asList(input.split(","));
+        Course course = Course.getCourse(inputList.get(0).trim());
+        Level level = Level.getLevel(inputList.get(1).trim());
+        Mission mission = Mission.getMission(level, inputList.get(2).trim());
+
+        List<String> crewNames = chooseCourse(course);
+
+        PairResult pairResult = shuffledCrew(crewNames, course, level, mission);
+
+        OutputView.printList(pairResult.getPairList());
+
+    }
+
+    private List<String> chooseCourse(Course course) {
+        List<Crew> backendCrews = fileScanner.loadBackendCrews();
+        List<Crew> frontendCrews = fileScanner.loadFrontendCrews();
+        if (course == Course.BACKEND) {
+            return backendCrews.stream()
+                    .map(Crew::getName)
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
+        return frontendCrews.stream()
+                .map(Crew::getName)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private PairResult shuffledCrew(List<String> crewNames, Course course, Level level, Mission mission) {
+        List<String> shuffledCrew = Randoms.shuffle(crewNames);
+        PairResult pairResult = new PairResult(course, level, mission);
+        for (int i = 0; i < shuffledCrew.size() - 1; i += 2) {
+            Pair pair = new Pair();
+            pair.add(shuffledCrew.get(i));
+            pair.add(shuffledCrew.get(i+1));
+            if (shuffledCrew.size() % 2 == 1 && i == shuffledCrew.size() - 2) {
+                pair.add(shuffledCrew.get(i+2));
+            }
+            pairResult.addPairResult(pair);
+        }
+        return pairResult;
+    }
+
+    private void searchInfo(PairResults pairResults) {
+        String input = InputView.readInfo();
+        List<String> inputList = Arrays.asList(input.split(","));
+        Course course = Course.getCourse(inputList.get(0).trim());
+        Level level = Level.getLevel(inputList.get(1).trim());
+        Mission mission = Mission.getMission(level, inputList.get(2).trim());
+        PairResult pairResult = pairResults.findByInfo(course, level, mission);
+        validateHistory(pairResult);
+
+        OutputView.printList(pairResult.getPairList());
+    }
+
+    private void validateHistory(PairResult pairResult) {
+        if (pairResult == null) {
+            throw new IllegalArgumentException(ErrorMessage.NON_MATCHING_HISTORY.getMessage());
+        }
+    }
+}

679329a16ec470210f29d1fded59e14cf9c278a0
전원준 <jwjjwj1960@naver.com>
2026-01-09 16:15:40 +0900

    feat: 결과출력 생성
    결과출력하는 로직 생성

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,7 +1,18 @@
 package pairmatching.view;
 
-public class OutputView {
-    public static void printList() {
+import java.util.List;
+import pairmatching.model.pair.Pair;
+import pairmatching.model.pair.PairResult;
+import pairmatching.model.pair.PairResults;
 
+public class OutputView {
+    public static void printList(List<Pair> pairs) {
+        System.out.println();
+        System.out.println("페어 매칭 결과입니다.");
+        for (Pair pair : pairs) {
+            String result = String.join(" : ", pair.getPair());
+            System.out.println(result);
+        }
+        System.out.println();
     }
 }

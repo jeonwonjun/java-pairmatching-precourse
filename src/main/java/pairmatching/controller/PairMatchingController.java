4af1428c0de29ce1491649d4f1fb5e9986d9bb44
전원준 <jwjjwj1960@naver.com>
2026-01-09 17:43:56 +0900

    feat: 이미 페어 경험이 있다면 3번까지 다시 입력받기 기능 추가(다시해야함)
    1. 모든 레벨에 대한 검증 필요
    2. List<String> 순서 조정해서 검증하는 법 필요
    3. 레벨별 정보가 저장되어야할지 판단 필요

--- src/main/java/pairmatching/controller/PairMatchingController.java
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -51,18 +51,36 @@ public class PairMatchingController {
     }
 
     private void makePair(PairResults pairResults) {
-        String input = InputView.readInfo();
-        List<String> inputList = Arrays.asList(input.split(","));
-        Course course = Course.getCourse(inputList.get(0).trim());
-        Level level = Level.getLevel(inputList.get(1).trim());
-        Mission mission = Mission.getMission(level, inputList.get(2).trim());
+        boolean ok = false;
+        int count = 0;
+        do {
+            count++;
+            String input = InputView.readInfo();
+            List<String> inputList = Arrays.asList(input.split(","));
+            Course course = Course.getCourse(inputList.get(0).trim());
+            Level level = Level.getLevel(inputList.get(1).trim());
+            Mission mission = Mission.getMission(level, inputList.get(2).trim());
+            PairResult periodResult = pairResults.findByInfo(course, level, mission);
 
-        List<String> crewNames = chooseCourse(course);
+            List<String> crewNames = chooseCourse(course);
 
-        PairResult pairResult = shuffledCrew(crewNames, course, level, mission);
+            PairResult pairResult = shuffledCrew(crewNames, course, level, mission);
 
-        OutputView.printList(pairResult.getPairList());
+            if (periodResult == null || !checkDuplicate(periodResult.getPairList(), pairResult.getPairList())) {
+                ok = true;
+                pairResults.addList(pairResult);
+                OutputView.printList(pairResult.getPairList());
+                break;
+            }
 
+            if (count == 3) {
+                throw new IllegalArgumentException(ErrorMessage.MATCHING_FAIL.getMessage());
+            }
+        } while (ok && count < 3);
+    }
+
+    private boolean checkDuplicate(List<Pair> periodResult, List<Pair> newResult) {
+        return periodResult.stream().anyMatch(newResult::contains);
     }
 
     private List<String> chooseCourse(Course course) {
@@ -81,12 +99,13 @@ public class PairMatchingController {
     private PairResult shuffledCrew(List<String> crewNames, Course course, Level level, Mission mission) {
         List<String> shuffledCrew = Randoms.shuffle(crewNames);
         PairResult pairResult = new PairResult(course, level, mission);
-        for (int i = 0; i < shuffledCrew.size() - 1; i += 2) {
+        for (int i = 0; i < shuffledCrew.size(); i += 2) {
             Pair pair = new Pair();
             pair.add(shuffledCrew.get(i));
             pair.add(shuffledCrew.get(i+1));
-            if (shuffledCrew.size() % 2 == 1 && i == shuffledCrew.size() - 2) {
+            if (i == shuffledCrew.size() - 3) {
                 pair.add(shuffledCrew.get(i+2));
+                i++;
             }
             pairResult.addPairResult(pair);
         }

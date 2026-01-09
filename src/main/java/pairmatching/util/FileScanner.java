8689fe0e5ea8e352bdb34e88dcbe41caaddea05e
전원준 <jwjjwj1960@naver.com>
2026-01-09 16:13:10 +0900

    feat: 파일 로드 후 반환값 추가
    기존에 file만 읽어서 List<String>으로 저장했는데 List를 Crew로 초기화해서 저장

--- src/main/java/pairmatching/util/FileScanner.java
+++ src/main/java/pairmatching/util/FileScanner.java
@@ -5,10 +5,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import pairmatching.model.crew.Course;
+import pairmatching.model.crew.Crew;
 
 public class FileScanner {
+    private static final String BACKEND_PATH = "src/main/resources/backend-crew.md";
+    private static final String FRONTEND_PATH = "src/main/resources/frontend-crew.md";
 
-    public static List<String> readFile(String filePath) {
+    public List<Crew> loadBackendCrews() {
+        List<String> lines = readFile(BACKEND_PATH);
+        return lines.stream()
+                .map(this::parseToBackendCrew)
+                .collect(Collectors.toList());
+    }
+
+    public List<Crew> loadFrontendCrews() {
+        List<String> lines = readFile(BACKEND_PATH);
+        return lines.stream()
+                .map(this::parseToFrontendCrew)
+                .collect(Collectors.toList());
+    }
+
+    private Crew parseToBackendCrew(String line) {
+        return new Crew(Course.BACKEND, line.trim());
+    }
+
+    private Crew parseToFrontendCrew(String line) {
+        return new Crew(Course.BACKEND, line.trim());
+    }
+
+    public List<String> readFile(String filePath) {
         try {
             Scanner scanner = new Scanner(new File(filePath));
             List<String> fileBody = new ArrayList<>();
